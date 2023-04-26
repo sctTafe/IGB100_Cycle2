50bf65791fe64f2b0f90918bfac1bf1a8b2dfaba
BlushingBlixer <takenakaeden@icloud.com>
2023-04-26 13:54:27 +1000

    Added Dustfly
    Feel free to scale and edit animation on the main game object if necessary. :)

---
+++ Assets/_Eden/Edens_Assets/Scripts/Dustfly_Wings.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dustfly_Wings : MonoBehaviour
+{
+    // create public for the wing flapping -E
+    public Transform leftWing;
+    public Transform rightWing;
+
+    // record current time in animation and how the wings move -E
+    float currentAnimTime = 0;
+    public float flapSpeed = 3;
+    public float flapAngle = 37;
+    public AnimationCurve flapCurve = AnimationCurve.EaseInOut(0, 0, 1, 1);
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentAnimTime += Time.deltaTime * flapSpeed;
+        leftWing.localEulerAngles = Vector3.back * ((flapCurve.Evaluate(currentAnimTime)*flapAngle*2)-flapAngle);
+        rightWing.localEulerAngles = -leftWing.eulerAngles;
+    }
+}
