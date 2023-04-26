613f27fea78373f719dd8bc524fc5fee657f3596
sctTafe <ba.scott.barley@gmail.com>
2023-04-27 07:18:27 +1000

    sct
    enemy stuff

---
+++ Assets/Master/Assets/Enemies/Enemy_Targeting.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Targeting : MonoBehaviour
+{
+
+
+
+
+}
+
+[Serializable]
+public class TargetingSetup
+{
+    public LayerMask _targetingLayer;
+    public float _targetingInRange;
+}
