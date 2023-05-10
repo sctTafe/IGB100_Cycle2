f21dddf175fa69a5817f7f2fbfb2cb2794412c2c
sctTafe <ba.scott.barley@gmail.com>
2023-05-10 18:02:22 +1000

    Some Tinkering & Balancing + SkyBox SFX
    Added Procedural Skybox
    Increased Ammo amount
    Made some more water sources & made them blue

---
+++ Assets/Master/Assets/VFX/VFX_ProcedualSkyBox/Ref - Tutorial.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 
+/// https://www.youtube.com/watch?v=Ueexx6Oh27Y
+/// 
+/// </summary>
+public class Ref_Tutorial : MonoBehaviour
+{
+
+}
